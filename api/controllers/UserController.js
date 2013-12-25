b03805264be7b990cb9c7b337fb3f3ae851b5840
Stanislav Gatev <s2gatev@gmail.com>
2013-12-25 22:07:47 +0200

    Add more actions to user

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -31,16 +31,105 @@ module.exports = {
 
         return res.redirect('/user/new');
       }
-      res.json(user);
+
+      res.redirect('/user/show/' + user.id);
     });
   },
 
-  toJSON: function() {
-    var obj = this.toObject();
-    delete obj.password;
-    delete obj.password_confirmation;
-    delete obj._csrf;
-    return obj;
-  }
+  show: function(req, res, next) {
+    User.findOne(req.param('id'), function foundUser(err, user) {
+      if (err) {
+        return next(err);
+      }
 
+      if (!user) {
+        return next();
+      }
+
+      res.view({
+        user: user
+      });
+    });
+  },
+
+  index: function(req, res, next) {
+    User.find(function foundUsers(err, users) {
+      if (err) {
+        return next(err);
+      }
+
+      res.view({
+        users: users
+      });
+    });
+  },
+
+  edit: function(req, res, next) {
+    User.findOne(req.param('id'), function foundUser(err, user) {
+      if (err) {
+        return next(err);
+      }
+
+      if (!user) {
+        return next('User doesn\'t exist.');
+      }
+
+      res.view({
+        user: user
+      });
+    });
+  },
+
+  update: function(req, res, next) {
+    if (req.session.User.admin) {
+      var userObj = {
+        name: req.param('name'),
+        title: req.param('title'),
+        email: req.param('email'),
+        admin: req.param('admin')
+      }
+    } else {
+      var userObj = {
+        name: req.param('name'),
+        title: req.param('title'),
+        email: req.param('email')
+      }
+    }
+
+    User.update(req.param('id'), userObj, function userUpdated(err) {
+      if (err) {
+        return res.redirect('/user/edit/' + req.param('id'));
+      }
+
+      res.redirect('/user/show/' + req.param('id'));
+    });
+  },
+
+  destroy: function(req, res, next) {
+
+    User.findOne(req.param('id'), function foundUser(err, user) {
+      if (err) {
+        return next(err);
+      }
+
+      if (!user) {
+        return next('User doesn\'t exist.');
+      }
+
+      User.destroy(req.param('id'), function userDestroyed(err) {
+        if (err) {
+          return next(err);
+        }
+
+        User.publishUpdate(user.id, {
+          name: user.name,
+          action: ' has been destroyed.'
+        });
+
+        User.publishDestroy(user.id);
+      });
+
+      res.redirect('/user');
+    });
+  },
 };
