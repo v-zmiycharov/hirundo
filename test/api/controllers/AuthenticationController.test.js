34f24a89f8dae7036fc5c44bf198599e5a97425c
Stanislav Gatev <s2gatev@gmail.com>
2013-12-30 19:39:32 +0200

    Add AuthenticationController tests

---
+++ test/api/controllers/AuthenticationController.test.js
@@ -0,0 +1,38 @@
+var test = require('../init');
+var controller = require('./init');
+
+describe('Authentication', function() {
+
+  describe('Routes', function() {
+
+    describe('Not logged', function(done) {
+
+      before(function() {
+        test.loggedOut();
+      });
+
+      it('POST /login should be redirected to /', function(done) {
+        controller.redirects('/login', '/', done);
+      }),
+
+      it('POST /logout should be redirected to /', function(done) {
+        controller.gets('/logout', done);
+      });
+    }),
+
+    describe('Logged', function(done) {
+
+      before(function() {
+        test.loggedAs(test.fixtures.users.Joe);
+      });
+
+      it('POST /login should return 200', function(done) {
+        controller.posts('/login', done);
+      }),
+
+      it('POST /logout should return 200', function(done) {
+        controller.redirects('/logout', '/', done);
+      });
+    });
+  });
+});
